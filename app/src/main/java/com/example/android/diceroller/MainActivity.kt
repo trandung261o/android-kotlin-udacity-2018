446bc247090fd95999e1b80e9149744006a55345
Tran Dinh Dung <trandungtk321@gmail.com>
2025-12-09 23:01:11 +0700

    1.16 findviewbyid()

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -17,12 +17,16 @@
 package com.example.android.diceroller
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.text = "Let's Roll"
+
     }
 }
