f64955e5a6e2b769805dc03829e371c568b60c56
Tran Dinh Dung <trandungtk321@gmail.com>
2025-12-11 12:30:59 +0700

    3.7 adding a TextView

--- chapter03-layouts/app/src/main/java/com/example/android/aboutme/MainActivity.kt
+++ chapter03-layouts/app/src/main/java/com/example/android/aboutme/MainActivity.kt
@@ -34,5 +34,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 }
